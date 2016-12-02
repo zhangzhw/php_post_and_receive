099f3b7c56b60157f35d2c1858576a7b484329bd
zhangzhw <zhangzhiwei_94@163.com>
2016-12-02 11:08:32 +0800

    json数据的post发送与接收处理

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+
+	$data = array (
+			"type"=>"123",
+		//	"pcPassword" => "601cadmin"
+	);
+	$url="http://192.168.1.103/gaotie/rec_device.php";
+	$data=json_encode($data);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	
+// 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+	
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//$data JSONÀàÐÍ×Ö·û´®
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+//	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data)));
+	
+	$filecontent=curl_exec($ch);
+	curl_close($ch);
+	echo $filecontent;
+
+?>
