e06e809309feecc186c23707ff6b0c15cafbda3d
zhangzhw <zhangzhiwei_94@163.com>
2016-12-02 11:53:58 +0800

    php curl实现json数据的post发送

--- test.php
+++ test.php
@@ -4,13 +4,13 @@ $data = array (
 			"type"=>"123"
 );
 $data=json_encode($data);
-//测试地址的url
+//鎺ユ敹鐢ㄧ殑URL
 $url="http://192.168.1.103/gaotie/rec_device.php";
 	
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data)));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//$data JSON类型字符串
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	
 $filecontent=curl_exec($ch);
