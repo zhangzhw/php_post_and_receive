4cd2170c888460b1bcd8cc99bb80013bf6de0927
zhangzhw <zhangzhiwei_94@163.com>
2017-06-20 03:18:38 -0700

    final demo

--- app.js
+++ app.js
@@ -6,6 +6,17 @@
 
 'use strict';
 
+require("appdynamics").profile({
+	controllerHostName: 'heroku-4709.saas.appdynamics.com',
+	controllerPort: 443, // If SSL, be sure to enable the next line
+	controllerSslEnabled: true,
+	accountName: 'heroku-4709',
+	accountAccessKey: '8k7fpd4zenz9',
+	applicationName: 'Applicationcontroller',
+	tierName: 'Tiercontroller',
+	nodeName: 'process' // The controller will automatically append the node name with a unique number
+});
+
 let express = require('express');
 let app = express();
 let server = require('http').Server(app);
